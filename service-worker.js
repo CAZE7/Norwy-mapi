6c54d62b6c6851f97ad64f73e9193c06fe2dd6ba
CAZE7 <jonas.rieckhoff@gmx.de>
2026-07-11 15:56:23 +0000

    Update service worker cache name and resources for version 13

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='norwegen-flat-v4';
-const SHELL=['./','index.html','app.css','leaflet.css','MarkerCluster.css','MarkerCluster.Default.css','leaflet.js','leaflet.markercluster.js','places-data.js','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+const CACHE='steder-single-v13';
+const APP=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
